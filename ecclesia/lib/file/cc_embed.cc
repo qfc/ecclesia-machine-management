3a7da7fd9150cb68757abcd30899ac853ee8a7a5
John Admanski <jadmanski@google.com>
2020-08-12 10:46:26 -0700

    Internal change
    PiperOrigin-RevId: 326264517

--- ecclesia/lib/file/cc_embed.cc
+++ ecclesia/lib/file/cc_embed.cc
@@ -17,16 +17,15 @@
 // This is a command-line utility that will generate a C++ header and source
 // file embedding a collection of files as raw bytes.
 //
-// The utility requires an output name and directory, a namespace, a variable
-// name, and a list of files to embed. It will then generate a .h and .cc file
+// The utility takes a list of input files. The files specified will be read in
+// and encoded into an EmbeddedFileArray data structure under their name,
+// possibly with some manipulations via the strip_prefixes and flatten
+// arguments.
+//
+// The utility also requires an output name and directory, a namespace, and a
+// variable name as flag parameters. It will then generate a .h and .cc file
 // with the given source name that defines a single variable of type
 // EmbeddedFileArray containing data from all of the given files.
-//
-// The data files will be embedded using the basename of the file, i.e. any
-// directories will be stripped off. So for example if you pass in an argument
-// of path/to/my/file.txt then it will be embedded as "file.txt". If multiple
-// files would be embedded with the same name then this utility will fail with
-// an error.
 
 #include <cstddef>
 #include <fstream>
@@ -40,21 +39,35 @@
 #include "absl/strings/escaping.h"
 #include "absl/strings/str_cat.h"
 #include "absl/strings/string_view.h"
+#include "absl/strings/strip.h"
 #include "absl/strings/substitute.h"
 #include "ecclesia/lib/file/path.h"
 #include "ecclesia/lib/logging/globals.h"
 #include "ecclesia/lib/logging/logging.h"
 
+// Flags that control the output files.
 ABSL_FLAG(std::string, output_name, "",
           "Name of the .cc and .h files that will be generated.");
 ABSL_FLAG(std::string, output_dir, ".",
           "Directory where the output files should be written.");
+
+// Flags that specify the symbols embedded into the output file.
 ABSL_FLAG(
     std::string, namespace, "",
     "The namespace the generated array variable will be inserted into. "
     "Multiple levels of nesting can be specified by using :: in the name.");
 ABSL_FLAG(std::string, variable_name, "",
           "The name of the EmbeddedFileArray variable.");
+
+// Flags that manipulate the paths of the output names.
+ABSL_FLAG(std::vector<std::string>, strip_prefixes, {},
+          "A list of prefix directories to strip off of each filename. If "
+          "multiple prefixes are specified then they'll be stripped off in the "
+          "order in which they were specified.");
+ABSL_FLAG(
+    bool, flatten, false,
+    "If specified then all directories will be stripped from output names, "
+    "effectively flattening the names into a single directory of files.");
 
 namespace ecclesia {
 namespace {
@@ -95,6 +108,24 @@ constexpr absl::string_view kSourceSuffixTemplate =
 }  // namespace $0
 )";
 
+// Given the path to an input file, use the strip_prefixes and flatten to
+// transform it into the name it should be embedded under.
+absl::string_view NameFromPath(absl::string_view path) {
+  // If --flatten is specified then we can just strip the path down to the
+  // basename and return that, no need to work through all the directory
+  // manipulation from --strip_prefixes.
+  if (absl::GetFlag(FLAGS_flatten)) {
+    return GetBasename(path);
+  }
+  // Otherwise we need to pull off all of the prefixes, one-by-one. We append a
+  // slash to each prefix to ensure that we're only stripping off a directory
+  // prefix and not an arbitrary string.
+  for (const std::string &prefix : absl::GetFlag(FLAGS_strip_prefixes)) {
+    absl::ConsumePrefix(&path, prefix + "/");
+  }
+  return path;
+}
+
 }  // namespace
 
 int RealMain(int argc, char *argv[]) {
@@ -111,15 +142,19 @@ int RealMain(int argc, char *argv[]) {
     FatalLog() << "variable_name was not specified";
   }
 
+  // Strip off the leading entry of args. The rest of the arguments are now the
+  // filenames to be embedded.
+  args.erase(args.begin());
+
   // Write out the header file. It contains nothing but an extern declaration
   // of the big array of data.
   //
   // We don't bother putting an include guard in the header. Having multiple
-  // copies of an extern declaration in the code doesn't cause any problems and
-  // so there isn't a ton of benefit to saving duplicate declarations. Adding
-  // an include guard would require adding yet another parameter to this binary
-  // and the caller would have to come up with a globally unique guard symbol.
-  // Omitting it simplifies the usage.
+  // copies of an extern declaration in the code doesn't cause any problems
+  // and so there isn't a ton of benefit to saving duplicate declarations.
+  // Adding an include guard would require adding yet another parameter to
+  // this binary and the caller would have to come up with a globally unique
+  // guard symbol. Omitting it simplifies the usage.
   {
     std::string header_path =
         JoinFilePaths(absl::GetFlag(FLAGS_output_dir),
@@ -127,12 +162,11 @@ int RealMain(int argc, char *argv[]) {
     std::fstream out_f(header_path, out_f.binary | out_f.trunc | out_f.out);
     if (!out_f.is_open()) FatalLog() << "unable to open " << header_path;
     out_f << absl::Substitute(kHeaderTemplate, absl::GetFlag(FLAGS_namespace),
-                              args.size() - 1,
-                              absl::GetFlag(FLAGS_variable_name));
+                              args.size(), absl::GetFlag(FLAGS_variable_name));
   }
 
-  // Write out the source file. The implementation we use is fairly simple. The
-  // source file just defines a single variable which is a std::array of
+  // Write out the source file. The implementation we use is fairly simple.
+  // The source file just defines a single variable which is a std::array of
   // string_view pairs. We fill it with data by reading in all the individual
   // files and turning them into huge string literals.
   {
@@ -142,29 +176,30 @@ int RealMain(int argc, char *argv[]) {
     std::fstream out_f(source_path, out_f.binary | out_f.trunc | out_f.out);
     if (!out_f.is_open()) FatalLog() << "unable to open " << source_path;
     out_f << absl::Substitute(kSourcePrefixTemplate,
-                              absl::GetFlag(FLAGS_namespace), args.size() - 1,
+                              absl::GetFlag(FLAGS_namespace), args.size(),
                               absl::GetFlag(FLAGS_variable_name));
-    // Track the filenames we've added.
-    absl::flat_hash_set<absl::string_view> filenames;
-    // Write out an entry for each file in args. We skip over the first argument
-    // because that's just the name of this binary.
+    // Track the names we've added.
+    absl::flat_hash_set<absl::string_view> input_names;
+    // Write out an entry for each file in args.
     std::string buffer(1024, '\0');
-    for (size_t i = 1; i < args.size(); ++i) {
-      std::fstream in_f(args[i], in_f.binary | in_f.in);
-      if (!in_f.is_open()) FatalLog() << "unable to open " << args[i];
-      // Make sure there's no duplicate filename.
-      absl::string_view filename = GetBasename(args[i]);
-      if (!filenames.insert(filename).second) {
-        FatalLog() << "multiple files named: " << filename;
+    for (const char *input_path : args) {
+      // Open up the input file.
+      std::fstream in_f(input_path, in_f.binary | in_f.in);
+      if (!in_f.is_open()) FatalLog() << "unable to open " << input_path;
+      // Make sure there's no duplicate name.
+      absl::string_view input_name = NameFromPath(input_path);
+      if (!input_names.insert(input_name).second) {
+        FatalLog() << "multiple entries named: " << input_name;
       }
-      // Start the EmbeddedFile entry and write out the filename. Note that we
-      // have to wrap all the data literals in an explicitly sized string_view
-      // constructor, or else the compiler will treat it as a NUL-terminated
-      // string causing any files with NUL in them will get truncated.
-      out_f << "{\"" << absl::CEscape(filename) << "\", absl::string_view(\n";
+      // Start the EmbeddedFile entry and write out the input name. Note that
+      // we have to wrap all the data literals in an explicitly sized
+      // string_view constructor, or else the compiler will treat it as a
+      // NUL-terminated string causing any files with NUL in them will get
+      // truncated.
+      out_f << "{\"" << absl::CEscape(input_name) << "\", absl::string_view(\n";
       // Read in the contents in buffer-sized chunks and copy them into the
-      // output file with appropriate escaping. We put one chunk per line rather
-      // than generating one huge line.
+      // output file with appropriate escaping. We put one chunk per line
+      // rather than generating one huge line.
       size_t data_size = 0;
       while (!in_f.eof()) {
         in_f.read(&buffer[0], buffer.size());
--- ecclesia/lib/file/cc_embed_interface_test.cc
+++ ecclesia/lib/file/cc_embed_interface_test.cc
@@ -18,11 +18,13 @@
 
 #include <array>
 #include <cstddef>
+#include <cstdint>
 
 #include "gtest/gtest.h"
 #include "absl/strings/string_view.h"
 #include "absl/types/optional.h"
 #include "ecclesia/lib/file/all_files.h"
+#include "ecclesia/lib/file/flat_files.h"
 #include "ecclesia/lib/file/text_files.h"
 
 namespace ecclesia {
@@ -33,21 +35,38 @@ namespace {
 
 TEST(EmbeddedFile, TextFileFromTextFiles) {
   ASSERT_EQ(kTextFiles.size(), 1);
-  EXPECT_EQ(kTextFiles[0].name, "text.txt");
+  EXPECT_EQ(kTextFiles[0].name, "test_data/text.txt");
   EXPECT_EQ(kTextFiles[0].data, "1-1A\n1-1A-2B\n1B-2B-3\n");
 }
 
 TEST(EmbeddedFile, TextFileFromAllFiles) {
   ASSERT_EQ(ecclesia_testdata::kAllFiles.size(), 2);
-  absl::string_view data =
-      GetEmbeddedFileWithNameOrDie("text.txt", ecclesia_testdata::kAllFiles);
+  absl::string_view data = GetEmbeddedFileWithNameOrDie(
+      "test_data/text.txt", ecclesia_testdata::kAllFiles);
   EXPECT_EQ(data, "1-1A\n1-1A-2B\n1B-2B-3\n");
 }
 
 TEST(EmbeddedFile, BinaryFileFromAllFiles) {
   ASSERT_EQ(ecclesia_testdata::kAllFiles.size(), 2);
-  absl::string_view data =
-      GetEmbeddedFileWithNameOrDie("blob.bin", ecclesia_testdata::kAllFiles);
+  absl::string_view data = GetEmbeddedFileWithNameOrDie(
+      "test_data/blob.bin", ecclesia_testdata::kAllFiles);
+  ASSERT_EQ(data.size(), 4096);
+  for (size_t i = 0; i < 4096; ++i) {
+    uint8_t data_byte = static_cast<uint8_t>(data[i]);
+    uint8_t expected_byte = static_cast<uint8_t>(i % 256);
+    EXPECT_EQ(data_byte, expected_byte);
+  }
+}
+
+TEST(EmbeddedFile, TextFileFromFlatFiles) {
+  ASSERT_EQ(kFlatFiles.size(), 2);
+  absl::string_view data = GetEmbeddedFileWithNameOrDie("text.txt", kFlatFiles);
+  EXPECT_EQ(data, "1-1A\n1-1A-2B\n1B-2B-3\n");
+}
+
+TEST(EmbeddedFile, BinaryFileFromFlatFiles) {
+  ASSERT_EQ(kFlatFiles.size(), 2);
+  absl::string_view data = GetEmbeddedFileWithNameOrDie("blob.bin", kFlatFiles);
   ASSERT_EQ(data.size(), 4096);
   for (size_t i = 0; i < 4096; ++i) {
     uint8_t data_byte = static_cast<uint8_t>(data[i]);
@@ -58,34 +77,42 @@ TEST(EmbeddedFile, BinaryFileFromAllFiles) {
 
 TEST(EmbeddedFile, GetWithValidName) {
   // These checks should never return nullopt.
-  EXPECT_NE(GetEmbeddedFileWithName("text.txt", kTextFiles), absl::nullopt);
-  EXPECT_NE(GetEmbeddedFileWithName("text.txt", ecclesia_testdata::kAllFiles),
+  EXPECT_NE(GetEmbeddedFileWithName("test_data/text.txt", kTextFiles),
             absl::nullopt);
-  EXPECT_NE(GetEmbeddedFileWithName("blob.bin", ecclesia_testdata::kAllFiles),
+  EXPECT_NE(GetEmbeddedFileWithName("test_data/text.txt",
+                                    ecclesia_testdata::kAllFiles),
+            absl::nullopt);
+  EXPECT_NE(GetEmbeddedFileWithName("test_data/blob.bin",
+                                    ecclesia_testdata::kAllFiles),
             absl::nullopt);
   // These checks should never die.
-  GetEmbeddedFileWithNameOrDie("text.txt", kTextFiles);
-  GetEmbeddedFileWithNameOrDie("text.txt", ecclesia_testdata::kAllFiles);
-  GetEmbeddedFileWithNameOrDie("blob.bin", ecclesia_testdata::kAllFiles);
+  GetEmbeddedFileWithNameOrDie("test_data/text.txt", kTextFiles);
+  GetEmbeddedFileWithNameOrDie("test_data/text.txt",
+                               ecclesia_testdata::kAllFiles);
+  GetEmbeddedFileWithNameOrDie("test_data/blob.bin",
+                               ecclesia_testdata::kAllFiles);
 }
 
 TEST(EmbeddedFile, GetWithBadName) {
-  EXPECT_EQ(GetEmbeddedFileWithName("blob.bin", kTextFiles), absl::nullopt);
-  EXPECT_EQ(GetEmbeddedFileWithName("blob.txt", ecclesia_testdata::kAllFiles),
+  EXPECT_EQ(GetEmbeddedFileWithName("test_data/blob.bin", kTextFiles),
             absl::nullopt);
-  EXPECT_EQ(GetEmbeddedFileWithName("text.bin", ecclesia_testdata::kAllFiles),
+  EXPECT_EQ(GetEmbeddedFileWithName("test_data/blob.txt",
+                                    ecclesia_testdata::kAllFiles),
+            absl::nullopt);
+  EXPECT_EQ(GetEmbeddedFileWithName("test_data/text.bin",
+                                    ecclesia_testdata::kAllFiles),
             absl::nullopt);
 }
 
 TEST(EmbeddedFileDeathTest, GetWithBadName) {
-  ASSERT_DEATH(GetEmbeddedFileWithNameOrDie("blob.bin", kTextFiles),
+  ASSERT_DEATH(GetEmbeddedFileWithNameOrDie("test_data/blob.bin", kTextFiles),
                "found no embedded file");
-  ASSERT_DEATH(
-      GetEmbeddedFileWithNameOrDie("blob.txt", ecclesia_testdata::kAllFiles),
-      "found no embedded file");
-  ASSERT_DEATH(
-      GetEmbeddedFileWithNameOrDie("text.bin", ecclesia_testdata::kAllFiles),
-      "found no embedded file");
+  ASSERT_DEATH(GetEmbeddedFileWithNameOrDie("test_data/blob.txt",
+                                            ecclesia_testdata::kAllFiles),
+               "found no embedded file");
+  ASSERT_DEATH(GetEmbeddedFileWithNameOrDie("test_data/text.bin",
+                                            ecclesia_testdata::kAllFiles),
+               "found no embedded file");
 }
 
 }  // namespace
